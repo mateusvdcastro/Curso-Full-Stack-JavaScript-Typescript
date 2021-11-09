3c052a5b7c312a74e32f0e17a8717527abd9b03b
mateusvdcastro <mateusvc1124@gmail.com>
2021-11-09 11:48:27 -0300

    Update

--- Aula 11-15.js
+++ Aula 11-15.js
@@ -33,3 +33,39 @@ console.log(umaString.split(' ', 2));  // vai dividir a string por espaços duas
 console.log(umaString.toUpperCase());
 console.log(umaString.toLowerCase());
 console.log(umaString.length)  // retorna o tamanho da string
+
+console.log("===========================NUMBERS==========================");
+
+let num1 = 0.7; // number
+let num2 = 0.1; // number
+
+// num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
+// num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.9
+// num1 = ((num1 * 100) + (num2 * 100)) / 100; // 1.0
+
+num1 = Number(num1.toFixed(2));  // arredondamento para 2 casas decimais 
+
+console.log(num1.toFixed(2));
+console.log(Number.isInteger(num1));
+
+
+// console.log(num1.toString() + num2);  // converter um número para string
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));   // ver se um num é um inteiro
+// console.log(Number.isNaN(temp));  // retorna true caso uma conta seja possível
+// let temp = num1 + '5';
+// console.log(temp);
+
+
+console.log(!!(100 / 0));
+
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));  // retorna a maior num
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+// const aleatorio = Math.round(Math.random());  // gera um num aleatório entre 0 e 1 
+console.log(Math.PI);
+console.log(Math.pow(2, 10));
